d716f0f88ad9a3ef1d8aef3d67700a61a8e770c1
«Aleksandr» <akhlopushin98@mail.ru>
2022-10-30 19:05:08 +0400

    решена задача 1

---
+++ dz_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class dz_1 {
+    
+    public static void main(String[] args) {
+        try (Scanner iScanner = new Scanner(System.in)) {
+            System.out.printf("Введите значение а: ");
+            int a = iScanner.nextInt();
+            System.out.printf("Введите значение b: ");
+            int b = iScanner.nextInt();
+            double c = Math.pow(a,b);
+            System.out.printf("a в степени b равно: ");
+            System.out.println(c);
+        }
+    }
+}
